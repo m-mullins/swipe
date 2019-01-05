e8a99017844020266f5ce97f9daae875f8754663
martin mullins <martin.mullins942@gmail.com>
2019-01-05 12:55:39 +1000

    sliders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import ReactSwing  from 'react-swing';
 import './App.css';
 
+
 class MyButton extends Component {
+
   handleClick = () => {
     this.props.onClick(this.props.id);
   }
@@ -18,6 +21,7 @@ class App extends Component {
     super(props);
     this.state  = {
       posts: null,
+      stack: null,
     };
   }
 
@@ -67,17 +71,56 @@ class App extends Component {
     this.setState({posts});
   }
 
+  handleThrowEnd = (el) => {
+      console.log(el.target.childNodes[0].id);
+      this.handleClick(el.target.childNodes[0].id);
+  }
+  
+  getComments = (postComments) => {
+    let commentIds = [];
+    let comments = [];
+    for (let comment of postComments) {
+      const { stickied, body, id } = comment.data;
+      if (stickied) { continue; }
+      if (commentIds.indexOf(id) >= 0) { continue; }
+      commentIds.push(id);
+      comments.push(<li>{body}</li>);
+    }
+
+    return comments;
+  }
+
   render() {
     return (
       <div className="App">
-          {this.state.posts && this.state.posts.map(page => {
+          {this.state.posts && <div id="viewport">
+              <ReactSwing
+                className="stack"
+                tagName="div"
+                setStack={stack => this.setState({ stack: stack })}
+                ref="stack"
+                throwout={e => { 
+                  console.log('throwout', e);
+                 }}
+                throwoutend={this.handleThrowEnd.bind(this)}
+                config={{
+                  throwOutConfidence: (xOffset, yOffset, element) => {
+                    const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth / 0.5, 1);
+                    const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight, 1);
+                
+                    return Math.max(xConfidence, yConfidence);
+                  }
+                }}
+
+              >
+            {this.state.posts && this.state.posts.map(page => {
               let postRender = null;
-              let comments = [];
-              let commentIds = [];
+              let contraComments = [];
+              let topComments = [];
               let postId = 1;
 
-              for (let p of page) {
-                if (postRender === null && p && p[0] && p[0].data && p[0].data.children[0]) {
+                let p = page[0];
+                if (p && p[0] && p[0].data && p[0].data.children[0]) {
                   const postData = p[0].data.children[0].data;
                   const { stickied, id, url, distinguished, thumbnail, title } = postData;
                   const isImage = url.match(/\.(jpeg|png|gif|jpg)$/) != null;
@@ -85,52 +128,50 @@ class App extends Component {
                   
                   if (stickied || distinguished === 'moderator' ||
                     !isImage || thumbnail === 'self') {
-                    continue;
+                    return <div key={id}></div>;
                   }
                   console.log(postData);
 
                   postRender =
-                    <div className="row" id={id} key={id}>
-                      <div className="col-md-2" key={`${id}-thumbnail`}>
-                        <img width="400" alt={id} src={url} />
-                      </div>
-                      <div className="col-md-2" key={`${id}-remove`}>
-                        <MyButton id={id} onClick={this.handleClick.bind(this)} />
-                      </div>
-
-                      <div className="col-md-auto" key={`${id}-title}`}>
+                    <div className="post" id={id} key={id}>
                         <p>{title}</p>
-                      </div>
+                        <img width="180" alt={id} src={url} />
+                        <MyButton id={id} onClick={this.handleClick.bind(this)} />
                     </div>;
                 }
 
                 if (p && p[1] && p[1].data && p[1].data.children) {
                   let postComments=p[1].data.children;
-                  for (let comment of postComments) {
-                    const { stickied, body, id } = comment.data;
-                    if (stickied) { continue; }
-                    if (commentIds.indexOf(id) >= 0) { continue; }
-                    commentIds.push(id);
-
-                    comments.push(
-                      <div className="row" id={id} key={id}>
-                        <div className="col-md-auto" >
-                          <p>{body}</p>
-                        </div>
-                    </div>);
-                  }
+                  contraComments = this.getComments(postComments);
+                } else {
+                  console.log("BBBB");
+                  console.log(p);
                 }
-              }
+
+                p = page[1];
+                if (p && p[1] && p[1].data && p[1].data.children) {
+                  let postComments=p[1].data.children;
+                  topComments = this.getComments(postComments);
+                } else {
+                  console.log("AAAA");
+                  console.log(p);
+                }
 
               return (
-                  <div key={`render-${postId}`}>
+                  <div className="card" ref={`render-${postId}`} key={`render-${postId}`}>
                     {postRender}
-                    {comments}
+                    <ul className="contra" key={`contra-${postId}`}>
+                      {contraComments}
+                    </ul>
+                    <ul className="top" key={`top-${postId}`}>
+                      {topComments}
+                    </ul>
                   </div>
                 );
-
             })}
-      </div>
+              </ReactSwing>
+        </div>}
+        </div>
     );
   }
 }
