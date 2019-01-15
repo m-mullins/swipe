8fed32f5063bf9e9db61c53d76c10ca30f26e56d
martin mullins <martin.mullins942@gmail.com>
2019-01-15 16:33:57 +1000

    add docs folder build

---
+++ docs/precache-manifest.9d4b3fe8538fc170bac8f27f88c99027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e5e204efadb6b96056",
+    "url": "/static/css/main.aef37569.chunk.css"
+  },
+  {
+    "revision": "13e5e204efadb6b96056",
+    "url": "/static/js/main.13e5e204.chunk.js"
+  },
+  {
+    "revision": "77a80a6cae58778944f4",
+    "url": "/static/js/1.77a80a6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "250e59e8d6dc3f116fb3c083971cebc4",
+    "url": "/index.html"
+  }
+];
