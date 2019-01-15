e15957cf462dbc27c264e586952cad53f4513a47
martin mullins <martin.mullins942@gmail.com>
2019-01-15 18:47:01 +1000

    add updated build

---
+++ docs/precache-manifest.da0a21085a691ebb8da71093c67bb72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d5edb5332801b212a41",
+    "url": "/swingforreddit/static/css/main.aef37569.chunk.css"
+  },
+  {
+    "revision": "3d5edb5332801b212a41",
+    "url": "/swingforreddit/static/js/main.3d5edb53.chunk.js"
+  },
+  {
+    "revision": "77a80a6cae58778944f4",
+    "url": "/swingforreddit/static/js/1.77a80a6c.chunk.js"
+  },
+  {
+    "revision": "e28c035419a1cef0f2c6",
+    "url": "/swingforreddit/static/js/runtime~main.e28c0354.js"
+  },
+  {
+    "revision": "912bcd932cdf7c63466226afebb123f4",
+    "url": "/swingforreddit/index.html"
+  }
+];
