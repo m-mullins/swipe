da89b843db69af251ceede8a6a1f94c7b9ad16ed
martin mullins <martin.mullins942@gmail.com>
2019-01-15 16:39:03 +1000

    fix bad docs folder

---
+++ docs/precache-manifest.ce9b740996276d53917ef5ab8c0cd70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec78f89e46d9f7425a64",
+    "url": "/static/css/main.aef37569.chunk.css"
+  },
+  {
+    "revision": "ec78f89e46d9f7425a64",
+    "url": "/static/js/main.ec78f89e.chunk.js"
+  },
+  {
+    "revision": "77a80a6cae58778944f4",
+    "url": "/static/js/1.77a80a6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f9d1c8f076094ed244516e237accc24",
+    "url": "/index.html"
+  }
+];
