39c83ef4b37d521e93d8b19f05d02bc08f68200e
martin mullins <martin.mullins942@gmail.com>
2019-01-18 23:44:52 +1000

    resort to none domain

---
+++ docs/precache-manifest.d7729f3f05ff8f8e7f2093f382161094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4eecbdaca2af0b75e2",
+    "url": "/swipe/static/css/main.d35890cf.chunk.css"
+  },
+  {
+    "revision": "af4eecbdaca2af0b75e2",
+    "url": "/swipe/static/js/main.af4eecbd.chunk.js"
+  },
+  {
+    "revision": "b82297829c6e0bcd60c4",
+    "url": "/swipe/static/js/1.b8229782.chunk.js"
+  },
+  {
+    "revision": "bc8cb547b745693bcc32",
+    "url": "/swipe/static/js/runtime~main.bc8cb547.js"
+  },
+  {
+    "revision": "1436b8b54a321c65b6bfc8b728825fe3",
+    "url": "/swipe/index.html"
+  }
+];
